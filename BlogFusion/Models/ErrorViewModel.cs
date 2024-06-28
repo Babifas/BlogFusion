1aa05662249e41a4a1b6ad0031f1a7b8d5b6a2ab
BABIFAS <babifasp@gmail.com>
2024-06-28 22:48:09 +0530

    Add project files.

---
+++ BlogFusion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogFusion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
