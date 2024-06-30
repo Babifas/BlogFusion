d45b15566187867ebfb13d4f23180717239d754f
BABIFAS <babifasp@gmail.com>
2024-07-01 01:13:11 +0530

    Database configuration completed.

---
+++ BlogFusion/Data/BlogFusionContext.cs
@@ -0,0 +1,18 @@
+﻿using BlogFusion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogFusion.Data
+{
+    public class BlogFusionContext : DbContext
+    {
+        public BlogFusionContext(DbContextOptions<BlogFusionContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- BlogFusion/Program.cs
+++ BlogFusion/Program.cs
@@ -1,7 +1,14 @@
+using BlogFusion.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DbContext
+builder.Services.AddDbContext<BlogFusionContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
